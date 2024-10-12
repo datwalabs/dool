2acad22b132e627b8bb052e47c9d26da4fd90404
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2024-10-12 12:30:48 +0530

    add user routes

---
+++ appservice/Services/UserService.py
@@ -0,0 +1,9 @@
+def hello():
+    return "Hello"
+
+
+def get_users():
+    return [1, 2, 3]
+
+def create_user(user):
+    pass
--- appservice/app.py
+++ appservice/app.py
@@ -1,11 +1,25 @@
 from flask import Flask
 
+# services
+import Services.UserService as UserService
+
 app = Flask(__name__)
 
 # Basic route for the homepage
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return UserService.hello()
+
+# User Management
+@app.route('/users', methods=['POST'])
+def create_user():
+    UserService.create_user(None)
+    return '', 201
+
+@app.route('/users')
+def get_users():
+    return UserService.hello()
+
 
 if __name__ == '__main__':
     # Run the app on localhost (127.0.0.1) with debug mode on
