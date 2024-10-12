8bf08a78c05f6280e1e08edd32454cd5ad06e260
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2024-10-12 13:13:16 +0530

    add job and oprator routes

--- appservice/app.py
+++ appservice/app.py
@@ -3,6 +3,8 @@ from flask import Flask
 # services
 import Services.UserService as UserService
 import Services.EnvironmentService as EnvService
+import Services.JobService as JobService
+import Services.OperatorService as OperatorService
 
 app = Flask(__name__)
 
@@ -27,7 +29,35 @@ def create_environment():
     EnvService.create_environment(None)
     return '', 201
 
+# Jobs Management
+@app.route('/jobs', methods=['POST'])
+def create_job():
+    JobService.create_job(None)
+    return '', 201
+
+@app.route('/jobs/tasks', methods=['POST'])
+def update_task():
+    JobService.update_task(None)
+    return '', 201
+
+# Operators
+@app.route('/operators')
+def get_operators():
+    return OperatorService.get_operators()
+
+# Runs
+@app.route('/jobs/runs')
+def get_all_job_runs():
+    return JobService.jobs_runs()
+
+@app.route('/jobs/running')
+def get_job_runnig():
+    return JobService.jobs_running()
+
+@app.route('/jobs/last-runs')
+def get_last_runs():
+    return JobService.get_last_runs()
 
 if __name__ == '__main__':
     # Run the app on localhost (127.0.0.1) with debug mode on
-    app.run(debug=True)
+    app.run(debug=True, host='192.168.29.167')
